4b57a7c301ffea4f0bf809a14fbf39e5386ee897
EmersonEMC <emerson.fac@gmail.com>
2022-03-19 14:32:29 -0300

    Add o interceptor de mensagens

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,14 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotifierModule } from 'angular-notifier';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './guards/auth.guard';
 import { AUTHINTERCEPTORPROVIDER } from './interceptors/auth.interceptor';
+import { MESSAGE_INTERCEPTORPROVIDER } from './interceptors/message.interceptor';
+import { customNotifierOptions } from './shared/components/notify/notify';
 
 registerLocaleData(ptBr);
 
@@ -22,10 +25,12 @@ registerLocaleData(ptBr);
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    NotifierModule.withConfig(customNotifierOptions),
   ],
   providers: [
     AuthGuard,
     AUTHINTERCEPTORPROVIDER,
+    MESSAGE_INTERCEPTORPROVIDER,
     { provide: LOCALE_ID, useValue: 'pt' },
   ],
   bootstrap: [AppComponent],
